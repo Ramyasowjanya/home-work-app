6bbae022504c6ecf5b254bcd1857db25606d8eea
Ramyasowjanya <ramyasowjanya.s@gmail.com>
2016-08-12 19:00:21 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -14,11 +14,11 @@ urlpatterns = [
     url(r'^list/menu/(?P<pk>[0-9]+)/delete/', classView.MenuDelete.as_view(), name='menudelete'),
     url(r'^list/menu/create/', classView.MenuCreateView.as_view(), name='menunew'),
     url(r'^list/update/',classView.RestauarntUpdateView.as_view(),name='restlistupdate'),
-    url(r'^lists/(?P<pk>[0-9]+)/menu/',classView.MenuList.as_view(),name='menulists'),
+    url(r'^(?P<pk>[0-9]+)/menu/',classView.MenuList.as_view(),name='menulists'),
     url(r'^list/menu/',classView.MenuDetail.as_view(),name='menulist'),
-    url(r'^lists/',classView.RestaurantList.as_view(),name='list'),
+    url(r'^',classView.RestaurantList.as_view(),name='list'),
     url(r'^list/',classView.RestaurantDetail.as_view(),name='loginlist'),
-    url(r'^',TemplateView.as_view(template_name="mainpage.html"),name="main"),
+    #url(r'^',TemplateView.as_view(template_name="mainpage.html"),name="main"),
 ]
 # urlpatterns=[
 #     url(r'^lists/(?P<pk>[0-9]+)/menu/', views.menus_detail),
